5881ae9f88fcfd47094b09de9e473bb3e541bdb7
kureta <skureta@gmail.com>
2016-09-29 04:12:41 +0300

    refactor counter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,18 @@
 import "bootstrap-sass";
 import "../styles/main.scss";
+
 import * as React from "react";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import * as ReactDOM from "react-dom";
 
-import Counter from "./components/Counter";
+import {Counter, reducer} from "./components/Counter";
+
+
+let store = createStore(reducer);
 
 ReactDOM.render(
-    <Counter />,
+    <Provider store={store}>
+        <Counter />
+    </Provider>,
     document.getElementById("injected-content"));
--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,50 +1,75 @@
 ﻿import * as React from "react";
-import {createStore} from "redux";
+import * as Redux from "redux";
+import {connect} from "react-redux";
 
-interface ICounterAction {
-    type: string;
+// Our increment action
+const INCR_ACTION = "INCR";
+interface IncrAction extends Redux.Action {
+    type: "INCR";
     by: number;
 }
 
-interface ICounterState {
+// Component properties that will be updated by the Redux state
+interface StateProps {
     counter: number;
 }
 
-let store = createStore(
-    (state: ICounterState, action: ICounterAction) => {
-        switch (action.type) {
-            case "INCR":
-                return {counter: state.counter + action.by};
-            default:
-                return state;
-        }
-    },
-    {counter: 0});
-
-export default class Counter extends React.Component<{}, ICounterState> {
-
-    private unsubscribe: Function;
-
-    public render() {
-        return (
-            <div>
-                <p>
-                    <label>Counter: </label><b>#{store.getState().counter}</b>
-                </p>
-                <button className="btn btn-primary" onClick={e => store.dispatch({ by: 1, type:"INCR" }) }>INCREMENT
-                </button>
-                <span style={{ padding: "0 5px" }}/>
-                <button className="btn btn-default" onClick={e => store.dispatch({ by: -1, type:"INCR"}) }>DECREMENT
-                </button>
-            </div>
-        );
-    }
-
-    private componentDidMount() {
-        this.unsubscribe = store.subscribe(() => this.forceUpdate());
-    }
-
-    private componentWillUnmount() {
-        this.unsubscribe();
-    }
+// Component properties that will dispatch a Redux state update
+interface DispatchProps {
+    onClick(val: number): IncrAction;
 }
+
+// All properties of the component merged together
+type CounterProps = StateProps & DispatchProps;
+
+// Increment action generator function, takes a value and returns an increment action
+const clickIncrement = function (incr: number): IncrAction {
+    return {
+        type: "INCR",
+        by: incr
+    };
+};
+
+// Reducer function. This guy takes the current staet and an action, and returns the new state
+export const reducer: Redux.Reducer<StateProps> = function (state: StateProps = {counter: 0}, action: IncrAction): StateProps {
+    switch (action.type) {
+        case INCR_ACTION:
+            return {counter: state.counter + action.by};
+        default:
+            return state;
+    }
+};
+
+const CounterView: React.StatelessComponent<{counter: number, onClick(val: number): IncrAction}> =
+    ({counter, onClick}) => (
+        <div>
+            <p>
+                <label>Counter: </label><b>#{counter}</b>
+            </p>
+            <button className="btn btn-primary" onClick={() => onClick(1)}>INCREMENT
+            </button>
+            <span style={{ padding: "0 5px" }}/>
+            <button className="btn btn-default" onClick={() => onClick(-1)}>DECREMENT
+            </button>
+        </div>
+    );
+
+function mapStateToProps(state: StateProps) {
+    return {
+        counter: state.counter
+    };
+}
+
+function mapDispatchToProps(dispatch: any) {
+    return {
+        onClick: (incr: number) => {
+            dispatch(clickIncrement(incr));
+        }
+    };
+}
+
+
+export const Counter = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CounterView);
